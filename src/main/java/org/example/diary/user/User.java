e1d2426d4b5ea0287b179643435190b209938d0b
jjsh0208 <jjsh0208@gmail.com>
2024-06-28 21:30:16 +0900

    2024-06-28

---
+++ src/main/java/org/example/diary/user/User.java
@@ -0,0 +1,39 @@
+package org.example.diary.user;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 30, nullable = false, unique = true)
+    private String email;
+
+    @Column(length = 15, nullable = false)
+    private String password;
+
+    @Column(length = 13, nullable = false)
+    private String phone;
+
+    @Column(length = 20, nullable = false)
+    private String name;
+
+    @Temporal(TemporalType.DATE)
+    @Column(nullable = false)
+    private Date birthDate;
+
+    @Column(length = 1, nullable = false)
+    private String sex;
+
+    @Column(length = 4, nullable = false)
+    private String mbti;
+}
---
+++ src/main/java/org/example/diary/user/UserController.java
@@ -0,0 +1,23 @@
+package org.example.diary.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/regist")
+    public ResponseEntity<String> register(@RequestBody UserRegisterDTO userDto) {
+        userService.save(userDto);
+        return ResponseEntity.ok("success");
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> longUser(@RequestBody UserLoginDTO userLoginDto) {
+        return userService.login(userLoginDto);
+    }
+}
