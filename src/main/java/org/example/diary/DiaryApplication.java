776f4d4b977bdaa268b682bb43b94b39af4f20bf
강수빈 <ksbkitty2@naver.com>
2024-07-01 10:36:25 +0900

    youtube_api : play 버튼과 select 버튼을 만들어 select 선택시 해당 영상의 url 팝업으로 띄우기 구현

--- src/main/java/org/example/diary/DiaryApplication.java
+++ src/main/java/org/example/diary/DiaryApplication.java
@@ -2,6 +2,8 @@ package org.example.diary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class DiaryApplication {
@@ -9,5 +11,4 @@ public class DiaryApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DiaryApplication.class, args);
 	}
-
 }
